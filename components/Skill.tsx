3d3662474e4975ae908e136283cc0bae6bac4046
zhwltlr <vyoungv7340@naver.com>
2023-12-22 00:00:20 +0900

    fixed skill page style

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -15,7 +15,7 @@ const Skill = ({ directionLeft, skills }: Props) => {
         transition={{ duration: 0.5 }}
         whileInView={{ opacity: 1, x: 0 }}
         src={`${skills.skillImageUrl}`}
-        className="object-cover w-24 h-24 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
+        className="object-cover w-24 h-24 md:w-28 md:h-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out"
       />
       <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white h-24 w-24 md:w-28 md:h-28 xl:w-32 xl:h-32 z-0">
         <div className="flex items-center justify-center h-full">
